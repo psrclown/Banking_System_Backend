18879a11c638188f62939364fa439a176125f388
Ansh Singh <anshpsr1122@gmail.com>
2026-02-12 23:16:45 +0530

    register api completed

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,5 +1,35 @@
 const userModel = require("../models/user.model");
-
-function userRegisterController(req, res) {
+const jwt = require("jsonwebtoken");
+/*
+user register controller
+/api/auth/register
+ * */
+async function userRegisterController(req, res) {
   const { email, password, name } = req.body;
+  const isExists = await userModel.findOne({ email: email });
+  if (isExists) {
+    return res.status(422).json({
+      message: "User already exists with this email",
+      status: "failed",
+    });
+  }
+  const user = await userModel.create({
+    email,
+    password,
+    name,
+  });
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "3d",
+  });
+  res.cookie("Token", token);
+  res.status(201).json({
+    user: {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    token,
+  });
 }
+
+module.exports = { userRegisterController };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,38 +1,40 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "Email is required for creating an account"],
-    trim: true,
-    lowercase: true,
-    match: [
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "invalid email address",
-    ],
-    unique: [true, "Email already exists"],
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required for creating an account"],
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "invalid email address",
+      ],
+      unique: [true, "Email already exists"],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required for creating an account"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required for creating an account"],
+      minlength: [6, "Password should contain more than 6 characters"],
+      select: false,
+    },
   },
-  name: {
-    type: String,
-    required: [true, "Name is required for creating an account"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required for creating an account"],
-    minlength: [6, "Password should contain more than 6 characters"],
-    select: false,
-  },
-  timestamps: true,
-});
+  { timestamps: true },
+);
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
   const hash = await bcrypt.hash(this.password, 10);
   this.password = hash;
-  return next();
+  return;
 });
 
 userSchema.methods.comparePassword = async function (password) {
