abb79ea627daa42da6eac884b843cba0f7546e73
Ansh Singh <anshpsr1122@gmail.com>
2026-02-14 13:14:10 +0530

    auth Middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRouter = require("./routes/auth.route");
 const cookieParser = require("cookie-parser");
+const accountRouter = require("./routes/accounts.route");
 
 const app = express();
 
@@ -8,4 +9,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
+
 module.exports = app;
---
+++ src/models/account.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "Account must be associated with user"],
+      index: true,
+    },
+    status: {
+      enum: {
+        values: ["ACTIVE", "FROZEN", "CLOSED"],
+        message: "Status can be either Active, Frozen or Closed",
+      },
+    },
+    curency: {
+      type: String,
+      required: [true, "Currency is required for creating an account"],
+      default: "INR",
+    },
+  },
+  { timestamps: true },
+);
+
+accountSchema.index({ user: 1, status: 1 });
+
+const accountModel = mongoose.model("account", accountSchema);
+
+module.exports = accountModel;
